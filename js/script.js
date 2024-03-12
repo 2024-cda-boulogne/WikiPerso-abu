5800676b631110ffc6ff48b8aac438d5db3cf1e0
JunkBezoul <alex.buffet7839@gmail.com>
2024-03-12 16:23:20 +0100

    Pop up working

--- js/script.js
+++ js/script.js
@@ -13,8 +13,32 @@ function myFunction() {
   }
 }
 
+
+
 let darkToggle = document.querySelector('#darkToggle');
 
 darkToggle.addEventListener('change', ()=> {
   document.body.classList.toggle('dark');
 })
+
+
+
+let modal = document.getElementById("myModal");
+
+let btn = document.getElementById("myBtn");
+
+let span = document.getElementsByClassName("close")[0];
+
+btn.onclick = function() {
+  modal.style.display = "block";
+}
+
+span.onclick = function() {
+  modal.style.display = "none";
+}
+
+window.onclick = function(event) {
+  if (event.target == modal) {
+    modal.style.display = "none";
+  }
+}
