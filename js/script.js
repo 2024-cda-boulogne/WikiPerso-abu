5ff03d9e285467b6948b7bcdc5380df6dc70f4bb
JunkBezoul <alex.buffet7839@gmail.com>
2024-03-24 14:27:13 +0100

    rendu final

--- js/script.js
+++ js/script.js
@@ -14,23 +14,6 @@ sun.addEventListener("change", toggleTheme);
 
 
 
-window.onscroll = function () {
-  nav();
-};
-
-let navbar = document.getElementById('navbar');
-let sticky = navbar.offsetTop;
-
-function nav() {
-  if (window.pageYOffset >= sticky) {
-    navbar.classList.add('sticky');
-  } else {
-    navbar.classList.remove('sticky');
-  }
-}
-
-
-
 
 let modal = document.getElementById("myModal");
 
